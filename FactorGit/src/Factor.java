d21c685747cc1a1d8af09844810355435c7bd8d4
jake100 <jake62779@gmail.com>
2019-09-24 21:00:48 -0700

    added bimomialFactor

--- FactorGit/src/Factor.java
+++ FactorGit/src/Factor.java
@@ -6,7 +6,50 @@ public class Factor {
 
     public Factor(String text)    
     {
-    	String pattern = "^([a-zA-Z])(\\d+) ([+-]) (-?)(\\d+)(\\1)(\\d+) ([+-]) (-?)(\\d+)(\\1) ([+-]) (\\d)$";
+    	String pattern = "^([a-zA-Z])(\\d+) ([+-]) (-?)(\\d+)(\\1)(\\d+) ([+-]) (-?)(\\d+)(\\1) ([+-]) (-?)(\\d)$";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(text);
+	    if (m.find())
+	    {
+	    	originalFactor(text);
+	    	
+	  
+	    }
+	    else 
+	    {
+	      	pattern = "^([a-zA-Z])(\\d+) ([+-]) (-?)(\\d+)(\\1)(\\d+)$";
+	    	r = Pattern.compile(pattern);
+	    	m = r.matcher(text);
+	    	if(m.find())
+	    	{
+	    		bimomialFactor(text);
+	    	}
+	    	else System.out.println("NO MATCH");
+	    }
+    }
+    public void bimomialFactor(String text)
+    {
+    	String pattern = "^([a-zA-Z])(\\d+) ([+-]) (-?)(\\d+)(\\1)(\\d+)$";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(text);
+	    if (m.find())
+	    {
+	    	System.out.println("found: " + m.group(1));
+	    	System.out.println("found: " + m.group(2));
+	    	System.out.println("found: " + m.group(3));
+	    	System.out.println("found: " + m.group(4));
+	    	System.out.println("found: " + m.group(5));
+	    	System.out.println("found: " + m.group(6));
+	    	System.out.println("found: " + m.group(7));
+	      }
+	    else 
+	    {
+	         System.out.println("NO MATCH");
+	    }
+    }
+    public void originalFactor(String text)
+    {
+    	String pattern = "^([a-zA-Z])(\\d+) ([+-]) (-?)(\\d+)(\\1)(\\d+) ([+-]) (-?)(\\d+)(\\1) ([+-]) (-?)(\\d)$";
         Pattern r = Pattern.compile(pattern);
         Matcher m = r.matcher(text);
 	    if (m.find())
@@ -24,6 +67,7 @@ public class Factor {
 	    	System.out.println("found: " + m.group(11));
 	    	System.out.println("found: " + m.group(12));
 	    	System.out.println("found: " + m.group(13));
+	    	System.out.println("found: " + m.group(14));
 	      }
 	    else 
 	    {
@@ -31,6 +75,6 @@ public class Factor {
 	    }
     }
     public static void main(String[] args) {
-		new Factor("x2 - -2x2 + -4x - 8");
+		new Factor("x2 - -2x2");
 	}
 }
